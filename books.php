ba8be71ad99ede9fe613f0631d4a35bad07b88ae
Jeff Codling <circuitjc@gmail.com>
2014-11-13 21:43:21 -0500

    Newest Changes
    Newest changes:
    Modified css for the look of books.
    Added single book view.
    Changed to localhost for database.

--- books.php
+++ books.php
@@ -6,6 +6,7 @@ include_once("php/constants.php");
 <html lang="en">
   <head>
 	<title>A Novel Concept</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
@@ -35,23 +36,26 @@ include_once("php/constants.php");
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
 					<h4 class="modal-title">Log-in</h4>
-				</div>
-				<div class="modal-body">
-					<div class="form-group">
-						<label for="exampleInputEmail1">Email address</label>
-						<input class="form-control" id="exampleInputEmail1" placeholder="Enter email" type="email">
 					</div>
-					<div class="form-group">
-						<label for="exampleInputPassword1">Password</label>
-						<input class="form-control" id="exampleInputPassword1" placeholder="Password" type="password">
+					<form action="" method="post">
+					<div class="modal-body">
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+							<input class="form-control" id="exampleInputEmail1" name="username" placeholder="Enter email" type="email">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input class="form-control" id="exampleInputPassword1" name="password" placeholder="Password" type="password">
+						</div>
+						<p class="text-left"><a href="forgot.php">Forgot password?</a></p>
+						<p class="text-right">New User? <a href="register.php">Sign Up</a> for a free account.</p>
 					</div>
-					<p class="text-left"><a href="#">Forgot password?</a></p>
-					<p class="text-right">New User? <a href="#">Sign Up</a> for a free account.</p>
-				</div>
-				<div class="modal-footer">
-					<a href="#" data-dismiss="modal" class="btn">Close</a>
-					<a href="member.php" class="btn btn-primary">Log-in</a>
-				</div>
+					<div class="modal-footer">
+						<a href="#" data-dismiss="modal" class="btn">Close</a>
+						<button type="submit" class="btn btn-default">Log-in</button>
+<!-- 						<a href="member.php" class="btn btn-primary">Log-in</a> -->
+					</div>
+					</form>
 		    </div>
 		</div>
 	</div>
@@ -71,61 +75,6 @@ include_once("php/constants.php");
 					<a href="checkout.php" class="btn btn-primary">Proceed to Checkout</a>
 				</div>
 		    </div>
-		</div>
-	</div>
-
-	<div class="modal fade" id="bookModal">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title">
-					<?php
-					if(isset($_REQUEST['book_id'])) {
-						$book_id = $_REQUEST['book_id'];
-					} else {
-						$book_id = 1;
-					}
-					$stmt = "SELECT b.book_id,book_title,book_plot,book_price,author_last,author_first,author_middle FROM books b";
-					$stmt = $stmt." JOIN book_authors j ON b.book_id=j.book_id JOIN authors a ON j.author_id=a.author_id";
-					$where = " WHERE b.book_id=".$book_id;
-					$orderby = "book_title";
-	
-					$stmt .= $where." ORDER BY ".$orderby;
-					$results = mysqli_query($con, $stmt);
-					if(mysqli_errno($con)) {
-						die("Could not select bookid ".$book_id.".<br>Query: ".$stmt."<br>Error: ".mysqli_error($con));
-					}
-
-					$book_title = $book_author = $book_plot = "---";
-					$book_price = 0;
-					if($row = mysqli_fetch_array($results)) {
-						$book_title = $row['book_title'];
-						$book_author = $row['author_first']." ".$row['author_middle']." ".$row['author_last'];
-						$book_plot = $row['book_plot'];
-						$book_price = $row['book_price'];
-					} else {
-						echo "Error parsing row.<br>";
-					}
-					echo $book_title;
-					echo "</h4></div>\n";
-					echo "<div class=\"modal-body\">";
-					echo "<img src=\"images/";
-					if(file_exists("images/".$book_id.".jpg")) {
-						echo $book_id;
-					} else {
-						echo "coverart";
-					}
-					echo ".jpg\">";
-					echo "<span class=\"singlebookdetails\">Author: ".$book_author."<br>";
-					echo "Plot: ".$book_plot."<br>";
-					printf("Price: $%.2f</span><br>",$book_price);
-					?>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-			</div>
 		</div>
 	</div>
 
@@ -181,7 +130,14 @@ include_once("php/constants.php");
 						<li><a href="checkout.php"><span class="glyphicon glyphicon-tag"></span> Checkout</a></li>
 					</ul>
 				</li>
-				<li><a href="#loginModal" data-toggle="modal" data-target="#loginModal"><span class="glyphicon glyphicon-user"></span> Log-in</a></li>
+				<?php
+				if(isset($_SESSION['clientid'])) {
+					echo "<li><span class=\"glyphicon glyphicon-user\"></span>".$_SESSION['clientname']."</li>";
+				} else {
+					?>
+					<li><a href="#loginModal" data-toggle="modal" data-target="#loginModal"><span class="glyphicon glyphicon-user"></span> Log-in</a></li>
+					<?php
+				}?>
 			</ul>
 		</div>
 	</nav>
@@ -190,39 +146,62 @@ include_once("php/constants.php");
 	$stmt = "SELECT b.book_id,book_title,book_plot,book_price,author_last,author_first,author_middle FROM books b";
 	$stmt = $stmt." JOIN book_authors j ON b.book_id=j.book_id JOIN authors a ON j.author_id=a.author_id";
 	$orderby = "book_title";
-	
-	$stmt .= " ORDER BY ".$orderby;
+	if(isset($_REQUEST['id']) && !isset($_REQUEST['back'])) {
+		$stmt .= " WHERE b.book_id=".$_REQUEST['id'];
+	} else {
+		$stmt .= " ORDER BY ".$orderby;
+	}
 	$results = mysqli_query($con, $stmt);
 	if(mysqli_errno($con)) {
 		die("Could not select books and authors.<br>Query: ".$stmt."<br>Error: ".mysqli_error($con));
 	}
-	echo "<div class=\"booklist\">\n";
-	$counter=1;
-	while($row = mysqli_fetch_array($results)) {
-		echo "<a href=\"?id=".$row['book_id']."\">";
-		echo "<div class=\"col-sm-6 col-md-4 col-lg-3\">";
-		echo "<h2><img src=\"images/";
+	if(isset($_REQUEST['id']) && !isset($_REQUEST['back'])) {
+		echo "<div class=\"singlebook clearfix\">";
+		$row = mysqli_fetch_assoc($results);
+		echo "<img src=\"images/";
 		if(file_exists("images/".$row['book_id'].".jpg")) {
 			echo $row['book_id'];
 		} else {
 			echo "coverart";
 		}
-		echo ".jpg\"></h2>\n";
-		echo "<h3>".$row['book_title']."</h3>\n";
-		echo "<p>".$row['author_first']." ".$row['author_last']."</p>";
-		echo "</div>\n";
-		if ($counter % 2 == 0) {
-			echo "<div class=\"clearfix visible-sm-block\"></div>";
+		echo ".jpg\">";
+		echo "<div class=\"booktitle\">".$row['book_title']."</div>";
+		echo "<div class=\"bookauthor\">".$row['author_first']." ".$row['author_middle']." ".$row['author_last']."</div>";
+		echo "<div class=\"bookplot\">".$row['book_plot']."</div>";
+		printf("<div class=\"bookprice\">$%.2f</div>",$row['book_price']);
+		echo "<form action=\"\" method=\"post\">";
+		echo "<input type=\"submit\" name=\"back\" value=\"Back\">";
+		echo "</form>";
+		echo "</div>";
+	} else {
+		echo "<div class=\"booklist\">\n";
+		$counter=1;
+		while($row = mysqli_fetch_array($results)) {
+			echo "<a href=\"?id=".$row['book_id']."\">";
+			echo "<div class=\"book col-xs-12 col-sm-6 col-md-4 col-lg-3\">";
+			echo "<h2><img src=\"images/";
+			if(file_exists("images/".$row['book_id'].".jpg")) {
+				echo $row['book_id'];
+			} else {
+				echo "coverart";
+			}
+			echo ".jpg\"></h2>\n";
+			echo "<h3>".$row['book_title']."</h3>\n";
+			echo "<p>".$row['author_first']." ".$row['author_last']."</p>";
+			echo "</div>\n";
+			if ($counter % 2 == 0) {
+				echo "<div class=\"clearfix visible-sm-block\"></div>";
+			}
+			if ($counter % 3 == 0) {
+				echo "<div class=\"clearfix visible-md-block\"></div>";
+			}
+			if ($counter % 4 == 0) {
+				echo "<div class=\"clearfix visible-lg-block\"></div>";
+			}
+			$counter++;
 		}
-		if ($counter % 3 == 0) {
-			echo "<div class=\"clearfix visible-md-block\"></div>";
-		}
-		if ($counter % 4 == 0) {
-			echo "<div class=\"clearfix visible-lg-block\"></div>";
-		}
-		$counter++;
+		echo "</div></a>\n";
 	}
-	echo "</div></a>\n";
 	?>
 	<?php
 	include_once("php/endofpage.php");
--- php/constants.php
+++ php/constants.php
@@ -1,9 +1,19 @@
 <?php
 // *********** PHP Server Credentials
-define("SERVERNAME","team01project.db.6194647.hostedresource.com");
-define("DATABASENAME","team01project");
-define("USR","team01project");
+define("SERVERNAME","gator3119.hostgator.com");
+define("DATABASENAME","rbp_team01");
+define("USR","rbp_team1");
 define("PASS","Pass4team01!");
+
+// define("SERVERNAME","localhost");
+// define("DATABASENAME","web_project");
+// define("USR","root");
+// define("PASS","");
+
+// define("SERVERNAME","team01project.db.6194647.hostedresource.com");
+// define("DATABASENAME","team01project");
+// define("USR","team01project");
+// define("PASS","Pass4team01!");
 
 // *********** PHP Settings Variable List
 $timelimit = 300;			// Amount of time in seconds that a client will stay logged in without doing something. (300 = 5 mins.)
